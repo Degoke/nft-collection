f7177a498268b6f7a597a0f012c96a09f3efed81
Adewoye Adegoke <69061153+Degoke@users.noreply.github.com>
2022-04-08 03:21:44 +0100

    Created project

---
+++ contracts/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xb6019702d257b90FF3d5Aa5222789e1d039c232B";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
