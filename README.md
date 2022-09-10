7b9763cceab8643aaf640fa09b1eeaf0f88e5ecb
Degoke <degokeadewoye@gmail.com>
2022-09-10 23:51:15 +0100

    switched to goerli

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nft-collection
  
+Crypto dev contract address 0x049646f1418842f612dbCF828Bf731071a7243DE
--- contracts/constants/index.js
+++ contracts/constants/index.js
@@ -1,5 +1,5 @@
-const WHITELIST_CONTRACT_ADDRESS = "0xb6019702d257b90FF3d5Aa5222789e1d039c232B";
+const WHITELIST_CONTRACT_ADDRESS = "0x5Bc229B6be2B895F1c01E83E03B27Dfab67B1CFf";
 
-const METADATA_URL = "https://nft-collection-drab-seven.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-degoke.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
